cef6f9a9a6dee426fc6eef737f5a881db75b20cf
aizej <davidxojd@gmail.com>
2026-08-28 09:22:11 +0200

    normal env settings

--- mujoco_playground/_src/locomotion/pendulum_inverse_pendulum/environment.py
+++ mujoco_playground/_src/locomotion/pendulum_inverse_pendulum/environment.py
@@ -27,7 +27,7 @@ def default_config() -> config_dict.ConfigDict:
                 upright=1.0,        # Tip height reward
                 control_cost=-0.001, # Penalize large torques
                 velocity_cost=-0.001, # Penalize fast swinging
-                continuity_cost=-1, # Penalize large changes in torque  
+                continuity_cost=-0.1, # Penalize large changes in torque  
             ),
         ),
         torque_randomisation_ratio = 1.15,
