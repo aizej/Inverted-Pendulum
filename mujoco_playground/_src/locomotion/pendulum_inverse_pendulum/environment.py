a9458377f7a9b0d4059fab18597f9c8cae880cbb
aizej <davidxojd@gmail.com>
2026-08-28 11:59:35 +0200

    smaller maximal delay 0.05 -> 0.02

--- mujoco_playground/_src/locomotion/pendulum_inverse_pendulum/environment.py
+++ mujoco_playground/_src/locomotion/pendulum_inverse_pendulum/environment.py
@@ -45,7 +45,7 @@ def default_config() -> config_dict.ConfigDict:
 
         obs_noise=config_dict.create(
             level=1.0,
-            observation_delay_max=0.005, #in seconds
+            observation_delay_max=0.002, #in seconds
             observation_delay_min=0.000,
             scales=config_dict.create(
                 joint_pos=0.002,
